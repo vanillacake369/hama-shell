d8d2d551452524c517ae032ad7499fef458546c1
limjihoon <lonelynight1026@gmail.com>
2025-11-02 18:40:16 +0900

    refactor : controller > 데몬/비데몬 책임 분리 및 동적 명령어 주입 지원

--- controller/daemon.go
+++ controller/daemon.go
@@ -1,14 +1,23 @@
 package controller
 
-import "fmt"
+import (
+	"fmt"
+	"hama-shell/core"
+)
 
 /*
-Daemonize 는 hama-shell 데몬 프로세스로써 수행되며 아래와 같은 책임을 수행한다.
-- config 변경점을 detect 하여 (elm architecture 기반)
-- profile 에 대한 pty 상태를 정적 파일로 관리 (~/.hama-shell/profile.json 으로 관리)
-- profile 에 대해 여러 서버 간 동일한 pty 를 공유하도록 ipc 처리
+Daemonize 는
+데몬 프로세스로써 수행되며
+아래와 같은 책임을 수행한다.
+- profile.yaml 변경 감시를 통해 profile.go 와 동기화
+- 여러 서버 간 동일한 pty 를 공유하도록 ipc 처리
+- TODO 1) 어떻게 하면 hama-shell 명령 시에 데몬프로세스와 명령어 처리 프로세스 두 개를 동시에 띄우지 ?
+- TODO 2) 위 두 가지 책임 이외에 데몬 프로세스가 책임져야 할 부분이 있을까?
 */
 func Daemonize() {
+	// profile.yaml 변경에 따른 DTO 동기화
+	core.SyncProfile()
+
 	// TODO : config 변경점을 어떻게 elm arch 방식으로 detection 하지 ?
 	fmt.Println("Daemonize() 함수 진입!!")
 }
